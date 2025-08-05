ada7fecaf49b815fd26d6b05fc6c6b82f987ca45
fidelis-talentsapartments <fidelisepeter@talentsapartments.com>
2025-08-05 09:46:57 +0100

    updated

--- middleware.ts
+++ middleware.ts
@@ -4,9 +4,9 @@ import { NextRequest, NextResponse } from "next/server";
 export function middleware(request: NextRequest) {
   const { searchParams, pathname } = request.nextUrl;
 
-  const title = searchParams.get("title");
-  const desc = searchParams.get("desc");
-  const cover = searchParams.get("cover");
+  const title = searchParams.get("title") ?? "none";
+  const desc = searchParams.get("desc") ?? "none";
+  const cover = searchParams.get("cover") ?? "none";
 
   if (pathname === "/" && title && desc && cover) {
     const newUrl = request.nextUrl.clone();
